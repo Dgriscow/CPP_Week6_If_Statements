7ee1ae1c78bc63aa2a9864ebbf7f09e7852970f8
Dgriscow <dgrischow@students.mchenry.edu>
2023-09-18 13:35:56 -0500

    preparing for hw

--- CPP_Week6_InclassDay1/HealthClubM.cpp
+++ CPP_Week6_InclassDay1/HealthClubM.cpp
@@ -1,47 +1,24 @@
 #include "HealthClubM.h"
 #include <iostream>
+#include <iomanip>
 using namespace std;
 void HealthClubM::displayMenu()
 {
+
 	do {
+
+
 		cout << "1: standard adult membership\n2:child membership\n3:senior membership\n4:Quit program\nEnter Your Choice: ";
-		cin >> choice;
+
 
 		//output formatting 
-		cout << fixed << showpoint << setprecision(2);
-
-
-		if (choice == 1) {
-			cout << "months?" << endl;
-			cin >> months;
-			charges = months * ADULT_RATE;
-			cout << "total Cost is: $" << charges << "\n\n";
-
-		}
-		else if (choice == 2) {
-			cout << "months?" << endl;
-			cin >> months;
-			charges = months * CHILD_RATE;
-			cout << "total Cost is: $" << charges << "\n\n";
-
-		}
-		else if (choice == 3) {
-			cout << "months?" << endl;
-			cin >> months;
-			charges = months * SENIOR_RATE;
-			cout << "total Cost is: $" << charges << "\n\n";
-
-
-		}
-		else if (choice == 4) {
+		if (choice == 4) {
 			cout << "exiting program";
-
-		}
-		else
-		{
+		}else {
 			cout << "invalid response";
-
 		}
+
+		
 
 	} while (choice != 4);
 
@@ -51,4 +28,44 @@ void HealthClubM::displayMenu()
 
 void HealthClubM::CalcCharges()
 {
+
+	cin >> choice;
+
+	cout << fixed << showpoint << setprecision(2);
+
+
+	if (choice == 1) {
+		cout << "months?" << endl;
+		cin >> months;
+		charges = months * ADULT_RATE;
+		cout << "total Cost is: $" << charges << "\n\n";
+
+	}
+	else if (choice == 2) {
+		cout << "months?" << endl;
+		cin >> months;
+		charges = months * CHILD_RATE;
+		cout << "total Cost is: $" << charges << "\n\n";
+
+	}
+	else if (choice == 3) {
+		cout << "months?" << endl;
+		cin >> months;
+		charges = months * SENIOR_RATE;
+		cout << "total Cost is: $" << charges << "\n\n";
+
+
+	}
+	else if (choice == 4) {
+		cout << "exiting program";
+
+	}
+	else
+	{
+		cout << "invalid response";
+
+	}
+
+
+
 }
--- CPP_Week6_InclassDay1/HealthClubM.h
+++ CPP_Week6_InclassDay1/HealthClubM.h
@@ -8,9 +8,17 @@ public:
 
 
 private:
-	int userChoice;
+	int choice, months;
 
 	void CalcCharges();
+
+
+	const double ADULT_RATE = 120.0;
+	const double CHILD_RATE = 60.0;
+	const double SENIOR_RATE = 100;
+
+	double charges = 0;
+
 
 
 
